76acb9de00386c7e02e0ae7f2d9941a9047a8345
Ehsan Samavati <samaavaati@gmail.com>
2022-12-11 10:28:48 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+// TODO add clean webpack plugin: so Delete dist folder before build(delete source maps)
+const config = {
+    entry: './src/index.ts',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
+
+module.exports = (env, argv) => {
+
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+    }
+
+    if (argv.mode === 'production') {
+        //...
+    }
+
+    return config;
+};
